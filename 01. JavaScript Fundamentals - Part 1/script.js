b56e6be184e120baa7aae69b6444c3c68ba8c6f1
anastasijasv <sviridova.anastasija@gmail.com>
2023-01-02 12:14:14 +0200

    added lesson 1

---
+++ 01. JavaScript Fundamentals - Part 1/script.js
@@ -0,0 +1,38 @@
+// Values and Variables
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+
+// Data Types
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
